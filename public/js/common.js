1a9be76c5a50c1fd32b7be730c96ecc0db7b75b0
liusu <471097828@qq.com>
2017-03-18 20:45:53 +0800

    JS代码的模块化设计

--- public/js/common.js
+++ public/js/common.js
@@ -1,13 +1,28 @@
-// 控制左侧导航菜单的显示和隐藏
-$('.navs ul').prev('a').on('click', function() {
-	$(this).next().slideToggle();
-});
-// var pathname = location.pathname;
-// 没有登录的时候跳转到登录页面
-var flag = $.cookie('PHPSESSID');
-var Url = location.pathname;
-if(Url != "/index/login") {
-	if(!flag) {
-		location.href = '/index/login';
+//模块化的处理的代码
+require.config({
+	baseUrl: '/public/',
+	paths: {
+		jquery: "assets/jquery/jquery.min",
+		bootstrap: "assets/bootstrap/js/bootstrap.min",
+		cookie: "assets/jquery-cookie/jquery.cookie",
+		nprogress: "assets/nprogress/nprogress",
+		echarts: "assets/echarts/echarts.min"
 	}
-}
+});
+
+require(["jquery", "cookie"], function($) {
+	
+	// 控制左侧导航菜单的显示和隐藏
+	$('.navs ul').prev('a').on('click', function() {
+		$(this).next().slideToggle();
+	});
+	// var pathname = location.pathname;
+	// 没有登录的时候跳转到登录页面
+	var flag = $.cookie('PHPSESSID');
+	var Url = location.pathname;
+	if(Url != "/index/login") {
+		if(!flag) {
+			location.href = '/index/login';
+		}
+	}
+})
---
+++ public/js/login.js
@@ -0,0 +1,29 @@
+require(["jquery", "cookie"], function($) {
+	console.log("login")
+	$('#loginForm').submit(function() {
+		var formData = $(this).serialize();
+		//后台交互数据进行数据交换
+		$.ajax({
+			type: 'post',
+			// url : 'http://api.studyit.com/login'
+			url: '/api/login',
+			data: formData,
+			dataType: 'json',
+			success: function(data) {
+				if(data.code == 200) {
+					// console.log(data.result);
+					var logInfo = JSON.stringify(data.result);
+					// 实现cookie数据的跨页面共享
+					$.cookie('logInfo', logInfo, { path: '/' });
+					location.href = '/index/index'
+				}
+			},
+			error: function(data) {
+				alert("用户名密码错误");
+				console.log(data.responseText);
+			}
+		});
+
+		return false; //阻止默认行为
+	});
+})
